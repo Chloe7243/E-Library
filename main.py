f87cf8aa7722f99cece344e3e1389f946fa7ebd4
Stephanie <Stephaniechloe21@gmail.com>
2023-05-01 09:47:43 +0100

    Admin pages and auth pages

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from flask import Flask
 from models import db
 from routes.auth import auth_bp
+from routes.views import views
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///models/library.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'your-secret-key'
 app.register_blueprint(auth_bp)
+app.register_blueprint(views)
 
 db.init_app(app)
 
-with app.app_context():
-    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
